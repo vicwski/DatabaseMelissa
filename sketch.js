873925353fc89d7f306e8b16d99f32ed5216f3a3
VicGA <vic.gajewski@gmail.com>
2022-06-08 17:54:39 -0300

    first commit

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var ball
+var database
+
+function setup() {
+  createCanvas(500, 500)
+
+  database = firebase.database()
+  console.log(database)
+
+  ball = createSprite(250, 250, 10, 10)
+  ball.shapeColor = 'red'
+}
+
+function draw() {
+  background('white')
+  if (keyDown(LEFT_ARROW)) {
+    changePosition(-1, 0)
+  } else if (keyDown(RIGHT_ARROW)) {
+    changePosition(1, 0)
+  } else if (keyDown(UP_ARROW)) {
+    changePosition(0, -1)
+  } else if (keyDown(DOWN_ARROW)) {
+    changePosition(0, +1)
+  }
+  drawSprites()
+}
+
+function changePosition(x, y) {
+  ball.x = ball.x + x
+  ball.y = ball.y + y
+}
